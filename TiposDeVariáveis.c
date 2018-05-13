3034ae831361e31663808d7af65a62f548720ed1
CodefailNerd <39227785+CodefailNerd@users.noreply.github.com>
2018-05-12 19:26:19 -0500

    Add files via upload

---
+++ TiposDeVariáveis.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+     //Definindo Variaveis
+     int a;
+     float b;
+     char c:
+     bool d:
+
+
+     //Passando Valores
+     a = 5;
+     b = 2.3;
+     c = 'a';
+     d = //true = 1; false = 0
+
+     //Escrvendo na Tela
+     printf("\nO valor da a = %d", a);
+     printf("\nO valor da b = %.1f", b);
+     printf("\nO valor da c = %c", c);
+     printf("\nO valor da c = %d\n", d);
+
+     //Lendo Valores
+     scanf("%d, &a);
+     scanf("%f, &b);
+     scanf(" %c", &c);
+     scanf(" %d", &d);
+
+     //Escrvendo na Tela
+     printf("\nO valor da a = %d", a);
+     printf("\nO valor da b = %.1f", b);
+     printf("\nO valor da c = %c\n", c);
+     printf("\nO valor da c = %d\n", d);
+
+     //Pausando
+     printf("pause");
+
+}
