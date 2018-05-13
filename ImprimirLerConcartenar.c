e059858aaaf9cc9c5bdb60a1c8a2754e04bd9d89
CodefailNerd <39227785+CodefailNerd@users.noreply.github.com>
2018-05-12 19:28:24 -0500

    Add files via upload

---
+++ ImprimirLerConcartenar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+     //Definindo uma Variavel
+     int a = 5;
+
+     //Imprimindo a Variavel "a"
+     printf("%d", a);
+
+     //Concartenando
+     printf ("O valor da var é": "%d", a);
+
+     //Pula linhas
+     printf("\n");
+
+     //Imprimindo
+     printf("\nE ai bro");
+
+     //Pausando
+     system("pause");
+
+}
